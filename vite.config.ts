3e4e4b56fbd095ea667106d2f4db24272a76df29
Nandana-pradeep33 <vanajapradeep999@gmail.com>
2024-06-17 00:25:19 +0530

    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Ensure this points to the correct directory
+  },
   plugins: [react()],
 })
