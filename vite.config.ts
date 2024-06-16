646d86059ffc8961bd89b27bb47a2678608e71b9
Nandana-pradeep33 <vanajapradeep999@gmail.com>
2024-06-17 01:07:01 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', // Ensure this points to the correct directory
-  },
+   base: "/TodoApp-Typescript/",
   plugins: [react()],
-  base: "/TodoApp/"
+ 
 })
